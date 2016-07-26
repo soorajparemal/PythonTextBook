e29b7e84f1abcae3b9baf5c1363916518adb0ed0
sooraj <srjps202@gmail.com>
2016-07-26 15:06:59 +0530

    pro

--- product.py
+++ product.py
@@ -1,3 +1,4 @@
+#To get user input as a list and to find the product of numbers inside
 num= input("Enter the numbers to be multiplied (no commas) : ")
 numbers = list(map(int , num.split()))
 def product(p):
