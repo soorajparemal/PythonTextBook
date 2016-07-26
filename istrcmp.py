c6a1c519a869b04bb57ef810773148a71af540c8
sooraj <srjps202@gmail.com>
2016-07-26 13:57:15 +0530

    simpleStringAddition

---
+++ istrcmp.py
@@ -0,0 +1,10 @@
+a=raw_input("Enter your Username :")
+b=raw_input("Re-enter your Username :")
+x= a.upper()
+y=b.upper()
+def istrcmp(x,y):
+	if x == y :
+		return True
+	else:
+		return False
+print istrcmp(x,y)
