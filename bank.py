e0da0ad4c58859c74e0c39c0372bf78205915b1c
sooraj <srjps202@gmail.com>
2016-08-09 05:46:05 +0530

    a

---
+++ bank.py
@@ -0,0 +1,10 @@
+class Bankaccounnt():
+	def __init__(self):
+		self.balance=0
+	def withdraw(self,amount):
+		self.balance -= amount
+		return self.balance
+	def deposit(self,amount):
+		self.balance += amount
+		return self.balance
+	
